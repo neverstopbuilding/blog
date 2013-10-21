bbed87f8a7b6d750659b940e434dfbfe34317958
Jason Fox <jasonrobertfox@gmail.com>
2013-10-21 18:46:57 -0300

    added the correct setting to the compess module to access the settings correctly from assets

--- src/_plugins/jekyll_asset_pipeline.rb
+++ src/_plugins/jekyll_asset_pipeline.rb
@@ -9,15 +9,20 @@ module JekyllAssetPipeline
   # process SCSS files
   class SassConverter < JekyllAssetPipeline::Converter
 
+
+    Compass.configuration.sass_dir = 'src/_assets/css'
+
     Compass.sass_engine_options[:load_paths].each do |path|
       Sass.load_paths << path
     end
+
 
     def self.filetype
       '.scss'
     end
 
     def convert
+
       Sass::Engine.new(@content, syntax: :scss).render
     end
   end
