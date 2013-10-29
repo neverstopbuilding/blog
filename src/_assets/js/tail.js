74b0a000f261aa8605363d718583345738d94492
Jason Fox <jasonrobertfox@gmail.com>
2013-10-29 01:44:12 -0300

    integrated pix panel and some basic tracking events

--- src/_assets/js/tail.js
+++ src/_assets/js/tail.js
@@ -1,1 +1,25 @@
 $(document).foundation();
+
+$('.end-of-article').waypoint(function(direction) {
+    if (direction == 'down') {
+        mixpanel.track("End of Article Reached", {
+            "title": $('.page').attr('name')
+        });
+    }
+}, { offset: '25%' });
+
+$('.recent-posts').waypoint(function(direction) {
+    if (direction == 'down') {
+        mixpanel.track("Recent Posts Reached", {
+            "title": $('.page').attr('name')
+        });
+    }
+}, { offset: '25%' });
+
+mixpanel.track_links("a.twitter", "Twitter Link Clicked", {'title': $('.page').attr('name') });
+
+mixpanel.track_links(".recent-posts a", "Recent Post Clicked", {'title': $('.page').attr('name') });
+
+mixpanel.track_links(".nav a", "Nav Item Clicked", function(link) {
+    return { nav_item: $(link).attr('name') }
+});
