80e41293ae986b518666f1c34234463fe65b8c25
Jason Fox <jasonrobertfox@gmail.com>
2014-03-22 15:47:09 -0300

    Add day of check.

--- src/_plugins/buffer.rb
+++ src/_plugins/buffer.rb
@@ -3,9 +3,18 @@ require 'buff'
 class Buffer < Jekyll::Generator
   def generate(site)
     if site.config['send_to_buffer']
-      message = generate_message(site)
-      log "Buffer message: \"#{message}\""
-      buffer(message, site)
+      post = most_recent_post(site)
+
+      post_date = post.date.strftime('%Y-%m-%d')
+      today_date = Date.today.strftime('%Y-%m-%d')
+      puts "#{post_date} #{today_date}"
+      if post_date == today_date
+        message = generate_message(post)
+        log "Buffer message: \"#{message}\""
+        buffer(message, site)
+      else
+        log 'Not sending latest promotion because older than today...'
+      end
     else
       log 'Not sending latest post promotion to Buffer...'
     end
@@ -17,12 +26,15 @@ class Buffer < Jekyll::Generator
     puts "\n\n#{message}\n\n"
   end
 
-  def generate_message(site)
-    most_recent_post = site.posts.reduce do |memo, post|
+  def generate_message(post)
+    promotion_message = post.data['promotion'] || ''
+    "#{promotion_message} #{post.location_on_server}"
+  end
+
+  def most_recent_post(site)
+    site.posts.reduce do |memo, post|
       memo.date > post.date ? memo : post
     end
-    promotion_message = most_recent_post.data['promotion'] || ''
-    "#{promotion_message} #{most_recent_post.location_on_server}"
   end
 
   def buffer(message, site)
@@ -31,7 +43,9 @@ class Buffer < Jekyll::Generator
     client = Buff::Client.new(access_token)
     content = { body: { text: message, top: true, shorten: true,
                         profile_ids: site.config['buffer_profiles'] } }
-    response = client.create_update(content)
+    # response = client.create_update(content)
+    puts client
+    response = content
     log("Buffer API Response: #{response.inspect}")
   end
 end
